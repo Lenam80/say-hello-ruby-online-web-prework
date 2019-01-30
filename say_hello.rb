d21c0c1d973607a1404afd90fcfdee20ccf356af
Angelina Montanez <angelinamontanez80@gmail.com>
2019-01-30 15:11:54 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # I call on the method, say_hello, and give it the string "Gabriela"
-say_hello("Gabriela")
+say_hello("Hello Ruby Programmer!","Gabriela")
 
 # The method prints this text to the screen:
 Hello Gabriela!# Build your say_hello method here
